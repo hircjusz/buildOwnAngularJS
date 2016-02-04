065c22c170f152a3362717326fafa3f9f7050d1a
hircjusz <darek.uchman@gmail.com>
2016-02-04 21:00:17 +0100

    NaNs

--- BuildOwnAngularJs/src/scope.js
+++ BuildOwnAngularJs/src/scope.js
@@ -6,10 +6,21 @@ function Scope() {
     
 }
 
-Scope.prototype.$watch = function (watchFn, listenerFn) {
+Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
+    if (valueEq) {
+        return _.isEqual(newValue, oldValue);
+    } else {
+        return newValue === oldValue ||
+        (typeof newValue === 'number' && typeof oldValue === 'number' &&
+        isNaN(newValue) && isNaN(oldValue));
+    }
+};
+
+Scope.prototype.$watch = function (watchFn, listenerFn,valueEq) {
     var watcher = {
         watchFn: watchFn,
-        listenerFn: listenerFn|| function() { },
+        listenerFn: listenerFn || function () { },
+        valueEq: !!valueEq,
         last: initWatchVal
     };
     this.$$watchers.push(watcher);
@@ -35,9 +46,9 @@ Scope.prototype.$$digestOnce = function () {
     _.forEach(this.$$watchers, function (watcher) {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
-        if (newValue !== oldValue) {
+        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
             self.$$lastDirtyWatch = watcher;
-            watcher.last = newValue;
+            watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
             watcher.listenerFn(newValue,(oldValue === initWatchVal ? newValue : oldValue), self);
             dirty = true;
         } else if (self.$$lastDirtyWatch === watcher) {
