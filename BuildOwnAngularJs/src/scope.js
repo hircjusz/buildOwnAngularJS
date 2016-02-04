c5a1c45d5aa4174be2f51f956702d8ff8c5f51d1
hircjusz <darek.uchman@gmail.com>
2016-02-04 19:58:43 +0100

    Keep Digesting While Dirty

--- BuildOwnAngularJs/src/scope.js
+++ BuildOwnAngularJs/src/scope.js
@@ -1,25 +1,38 @@
-﻿function Scope() {
+﻿function initWatchVal() { }
+
+function Scope() {
     this.$$watchers = [];
+    
 }
 
 Scope.prototype.$watch = function (watchFn, listenerFn) {
     var watcher = {
         watchFn: watchFn,
-        listenerFn: listenerFn
+        listenerFn: listenerFn|| function() { },
+        last: initWatchVal
     };
     this.$$watchers.push(watcher);
 };
 
-Scope.prototype.$digest = function () {
+Scope.prototype.$digest = function() {
+    var dirty;
+    do {
+        dirty = this.$$digestOnce();
+    } while (dirty);
+}
+
+Scope.prototype.$$digestOnce = function () {
     var self = this;
-    var newValue, oldValue;
+    var newValue, oldValue,dirty;
     
     _.forEach(this.$$watchers, function (watcher) {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
         if (newValue !== oldValue) {
             watcher.last = newValue;
-            watcher.listenerFn(newValue, oldValue, self);
+            watcher.listenerFn(newValue,(oldValue === initWatchVal ? newValue : oldValue), self);
+            dirty = true;
         }
     });
+    return dirty;
 };
--- BuildOwnAngularJs/test/scope_test1.js
+++ BuildOwnAngularJs/test/scope_test1.js
@@ -24,24 +24,58 @@ describe("Scope", function () {
             scope.$digest();
             expect(listenerFn).toHaveBeenCalled();
         });
-    });
-    
-    it("calls the listener function when the watched value changes", function () {
-        var scope = new Scope();
-        scope.someValue = 'a';
-        scope.counter = 0;
-        scope.$watch(
-        function (obj) { return obj.someValue; },
-        function (newValue, oldValue, obj) { obj.counter++; }
-        );
-        expect(scope.counter).toBe(0);
-        scope.$digest();
-        expect(scope.counter).toBe(1);
-        scope.$digest();
-        expect(scope.counter).toBe(1);
-        scope.someValue = 'b';
-        expect(scope.counter).toBe(1);
-        scope.$digest();
-        expect(scope.counter).toBe(2);
+
+
+        it("calls the listener function when the watched value changes", function () {
+            var scope = new Scope();
+            scope.someValue = 'a';
+            scope.counter = 0;
+            scope.$watch(
+            function (obj) { return obj.someValue; },
+            function (newValue, oldValue, obj) { obj.counter++; }
+            );
+            expect(scope.counter).toBe(0);
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+            scope.someValue = 'b';
+            expect(scope.counter).toBe(1);
+            scope.$digest();
+            expect(scope.counter).toBe(2);
+        });
+
+
+        it("may have watchers that omit the listener function", function () {
+            var watchFn = jasmine.createSpy().and.returnValue('something');
+            scope.$watch(watchFn);
+            scope.$digest();
+            expect(watchFn).toHaveBeenCalled();
+        });
+
+        it("triggers chained watchers in the same digest", function () {
+            scope.name = 'Jane';
+            scope.$watch(
+            function (scope) { return scope.nameUpper; },
+            function (newValue, oldValue, scope) {
+                if (newValue) {
+                    scope.initial = newValue.substring(0, 1) + '.';
+                }
+            }
+            );
+            scope.$watch(
+            function (scope) { return scope.name; },
+            function (newValue, oldValue, scope) {
+                if (newValue) {
+                    scope.nameUpper = newValue.toUpperCase();
+                }
+            }
+            );
+            scope.$digest();
+            expect(scope.initial).toBe('J.');
+            scope.name = 'Bob';
+            scope.$digest();
+            expect(scope.initial).toBe('B.');
+        });
     });
 });
