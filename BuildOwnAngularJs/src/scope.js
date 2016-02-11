3723278a1cb63bae142a1e871ef8e7a1e2275d5a
hircjusz <darek.uchman@gmail.com>
2016-02-11 21:34:41 +0100

    Handling exception

--- BuildOwnAngularJs/src/scope.js
+++ BuildOwnAngularJs/src/scope.js
@@ -28,7 +28,7 @@ Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
     }
 };
 
-Scope.prototype.$watch = function (watchFn, listenerFn,valueEq) {
+Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
     var watcher = {
         watchFn: watchFn,
         listenerFn: listenerFn || function () { },
@@ -39,7 +39,7 @@ Scope.prototype.$watch = function (watchFn, listenerFn,valueEq) {
     this.$$lastDirtyWatch = null;
 };
 
-Scope.prototype.$digest = function() {
+Scope.prototype.$digest = function () {
     var ttl = 10;
     var dirty;
     this.$$lastDirtyWatch = null;
@@ -52,8 +52,12 @@ Scope.prototype.$digest = function() {
 
     do {
         while (this.$$asyncQueue.length) {
-            var asyncTask = this.$$asyncQueue.shift();
-            asyncTask.scope.$eval(asyncTask.expression);
+            try {
+                var asyncTask = this.$$asyncQueue.shift();
+                asyncTask.scope.$eval(asyncTask.expression);
+            } catch (e) {
+                console.error(e);
+            }
         }
 
         dirty = this.$$digestOnce();
@@ -65,29 +69,37 @@ Scope.prototype.$digest = function() {
     this.$clearPhase();
 
     while (this.$$postDigestQueue.length) {
-        this.$$postDigestQueue.shift()();
+        try {
+            this.$$postDigestQueue.shift()();
+        } catch (e) {
+            console.error(e);
+        }
     }
 }
 
 Scope.prototype.$$digestOnce = function () {
     var self = this;
-    var newValue, oldValue,dirty;
-    
+    var newValue, oldValue, dirty;
+
     _.forEach(this.$$watchers, function (watcher) {
-        newValue = watcher.watchFn(self);
-        oldValue = watcher.last;
-        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
-            self.$$lastDirtyWatch = watcher;
-            watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
-            watcher.listenerFn(newValue,(oldValue === initWatchVal ? newValue : oldValue), self);
-            dirty = true;
-        } else if (self.$$lastDirtyWatch === watcher) {
-            return false;
+        try {
+            newValue = watcher.watchFn(self);
+            oldValue = watcher.last;
+            if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+                self.$$lastDirtyWatch = watcher;
+                watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+                watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);
+                dirty = true;
+            } else if (self.$$lastDirtyWatch === watcher) {
+                return false;
+            }
+        } catch (e) {
+            console.error(e);
         }
     });
     return dirty;
 };
-Scope.prototype.$applyAsync = function(expr) {
+Scope.prototype.$applyAsync = function (expr) {
     var self = this;
     self.$$applyAsyncQueue.push(function () {
         self.$eval(expr);
@@ -101,7 +113,11 @@ Scope.prototype.$applyAsync = function(expr) {
 
 Scope.prototype.$$flushApplyAsync = function () {
     while (this.$$applyAsyncQueue.length) {
-        this.$$applyAsyncQueue.shift()();
+        try {
+            this.$$applyAsyncQueue.shift()();
+        } catch (e) {
+            console.error(e);
+        }
     }
     this.$$applyAsyncId = null;
 };
