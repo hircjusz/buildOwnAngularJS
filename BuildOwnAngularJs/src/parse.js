b6c767fb9fb95fb03beaf129bc1898d9e2613d8b
hircjusz <darek.uchman@gmail.com>
2016-02-19 10:45:07 +0100

    parsing Int

--- BuildOwnAngularJs/src/parse.js
+++ BuildOwnAngularJs/src/parse.js
@@ -10,17 +10,63 @@ function Lexer() {
 }
 
 Lexer.prototype.lex = function (text) {
-    // Tokenization will be done here
+    this.text = text;
+    this.index = 0;
+    this.ch = undefined;
+    this.tokens = [];
+
+
+    while (this.index < this.text.length) {
+        this.ch = this.text.charAt(this.index);
+
+        if (this.isNumber(this.ch)) {
+            this.readNumber();
+        } else {
+            throw 'Unexpected next character' + this.ch;
+        }
+    }
+    return this.tokens;
+};
+
+Lexer.prototype.isNumber = function(ch) {
+    return '0' <= ch && ch <= '9';
+};
+
+Lexer.prototype.readNumber = function () {
+    var number = '';
+    while (this.index < this.text.length) {
+        var ch = this.text.charAt(this.index);
+        if (this.isNumber(ch)) {
+            number += ch;
+        } else {
+            break;
+        }
+        this.index++;
+    }
+
+    this.tokens.push({
+        text: number,
+        value: Number(number)
+    });
 };
 
 
 function AST(lexer) {
     this.lexer = lexer;
 }
+AST.Program = 'Program';
+AST.Literal = 'Literal';
+
+AST.prototype.program = function () {
+    return { type: AST.Program, body: this.constant() };
+};
+AST.prototype.constant = function () {
+    return { type: AST.Literal, value: this.tokens[0].value };
+};
 
 AST.prototype.ast = function (text) {
     this.tokens = this.lexer.lex(text);
-    // AST building will be done here
+    return this.program();
 };
 
 
@@ -29,9 +75,19 @@ function ASTCompiler(astBuilder) {
 }
 ASTCompiler.prototype.compile = function (text) {
     var ast = this.astBuilder.ast(text);
-    // AST compilation will be done here
+    this.state = { body: [] };
+    this.recurse(ast);
+    return new Function(this.state.body.join(''));
 };
 
+ASTCompiler.prototype.recurse = function (ast) {
+    switch (ast.type) {
+        case AST.Program:
+            this.state.body.push('return ', this.recurse(ast.body), ';');
+        case AST.Literal:
+            return ast.value;
+    }
+};
 
 function Parser(lexer) {
     this.lexer = lexer;
---
+++ BuildOwnAngularJs/test/parse_spec.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../Scripts/jasmine.js" />
+/// <reference path="../src/parse.js" />
+/// <reference path="../lib/loodash.js" />
+
+'use strict';
+
+describe("parse", function () {
+    it("can parse an integer", function () {
+        var fn = parse('42');
+        expect(fn).toBeDefined();
+        expect(fn()).toBe(42);
+    });
+});
