c7e31a53defeac7e93655f4387689896208a6c27
hircjusz <darek.uchman@gmail.com>
2016-02-20 13:38:00 +0100

    read Number

--- BuildOwnAngularJs/src/parse.js
+++ BuildOwnAngularJs/src/parse.js
@@ -19,16 +19,39 @@ Lexer.prototype.lex = function (text) {
     while (this.index < this.text.length) {
         this.ch = this.text.charAt(this.index);
 
-        if (this.isNumber(this.ch) ||
-  (this.ch === '.' && this.isNumber(this.peek()))) {
+        if (this.isNumber(this.ch) ||(this.ch === '.' && this.isNumber(this.peek()))) {
             this.readNumber();
-        } else {
+        }
+        else if (this.ch === '\'' || this.ch === '"') {
+            this.readString(this.ch);
+        }
+        else {
             throw 'Unexpected next character' + this.ch;
         }
     }
     return this.tokens;
 };
 
+
+Lexer.prototype.readString = function(quote) {
+    this.index++;
+    var string = '';
+    while (this.index < this.text.length) {
+        var ch = this.text.charAt(this.index);
+        if (ch === quote) {
+            this.index++;
+            this.tokens.push({
+                text: string,
+                value: string
+            });
+            return;
+        } else {
+            string += ch;
+        }
+        this.index++;
+    }
+    throw 'Unmatched quote';
+}
 Lexer.prototype.isNumber = function(ch) {
     return '0' <= ch && ch <= '9';
 };
@@ -108,7 +131,15 @@ ASTCompiler.prototype.recurse = function (ast) {
         case AST.Program:
             this.state.body.push('return ', this.recurse(ast.body), ';');
         case AST.Literal:
-            return ast.value;
+            return this.escape(ast.value);
+    }
+};
+
+ASTCompiler.prototype.escape = function (value) {
+    if (_.isString(value)) {
+        return '\'' + value + '\'';
+    } else {
+        return value;
     }
 };
 
