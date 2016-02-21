6cf60c37919ebd5fa39dcaf4a4b60d1563b4fe66
hircjusz <darek.uchman@gmail.com>
2016-02-21 16:38:59 +0100

    parsing nested keys

--- BuildOwnAngularJs/src/parse.js
+++ BuildOwnAngularJs/src/parse.js
@@ -36,7 +36,7 @@ Lexer.prototype.lex = function (text) {
             this.readIdent();
         } else if (this.isWhitespace(this.ch)) {
             this.index++;
-        } else if (this.is('[],{}:')) {
+        } else if (this.is('[],{}:.')) {
             this.tokens.push({ text: this.ch });
             this.index++;
         } else {
@@ -170,23 +170,35 @@ AST.ObjectExpression = 'ObjectExpression';
 AST.Property = 'Property';
 AST.Identifier = 'Identifier';
 AST.ThisExpression = 'ThisExpression';
-
+AST.MemberExpression = 'MemberExpression';
 
 
 AST.prototype.program = function () {
     return { type: AST.Program, body: this.primary() };
 };
 AST.prototype.primary = function () {
+    var primary;
     if (this.expect('[')) {
-        return this.arrayDeclaration();
+        primary= this.arrayDeclaration();
     } else if (this.expect('{')) {
-        return this.object();
+        primary= this.object();
     } else if (this.constants.hasOwnProperty(this.tokens[0].text)) {
-        return this.constants[this.consume().text];
+        primary= this.constants[this.consume().text];
     } else if (this.peek().identifier) {
-        return this.identifier();
+        primary= this.identifier();
     } else
-        return this.constant();
+        primary = this.constant();
+
+    while (this.expect('.')) {
+        primary= {
+            type: AST.MemberExpression,
+            object: primary,
+            property:this.identifier()
+        }
+
+    }
+
+    return primary;
 };
 
 AST.prototype.constant = function () {
@@ -281,6 +293,7 @@ ASTCompiler.prototype.compile = function (text) {
 };
 
 ASTCompiler.prototype.recurse = function (ast) {
+    var intoId;
     var self = this;
     switch (ast.type) {
         case AST.Program:
@@ -302,11 +315,16 @@ ASTCompiler.prototype.recurse = function (ast) {
             }, this);
             return '{' + properties.join(',') + '}';
         case AST.Identifier:
-            var intoId = this.nextId();
+            intoId = this.nextId();
             this.if_('s', this.assign(intoId, this.nonComputedMember('s', ast.name)));
             return intoId;
         case AST.ThisExpression:
             return 's';
+        case AST.MemberExpression:
+            intoId = this.nextId();
+            var left = this.recurse(ast.object);
+            this.if_(left,this.assign(intoId, this.nonComputedMember(left, ast.property.name)));
+            return intoId;
     }
 };
 ASTCompiler.prototype.nextId = function () {
