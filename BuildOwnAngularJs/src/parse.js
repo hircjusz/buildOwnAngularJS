70abb79c8479b3f21689dc83dc9418515731eca0
hircjusz <darek.uchman@gmail.com>
2016-02-21 16:25:07 +0100

    code refactoring for parsing attribute lookup

--- BuildOwnAngularJs/src/parse.js
+++ BuildOwnAngularJs/src/parse.js
@@ -181,7 +181,7 @@ AST.prototype.primary = function () {
         return this.object();
     } else if (this.constants.hasOwnProperty(this.tokens[0].text)) {
         return this.constants[this.consume().text];
-    }else if (this.peek().identifier) {
+    } else if (this.peek().identifier) {
         return this.identifier();
     } else
         return this.constant();
@@ -271,9 +271,10 @@ function ASTCompiler(astBuilder) {
 }
 ASTCompiler.prototype.compile = function (text) {
     var ast = this.astBuilder.ast(text);
-    this.state = { body: [] };
+    this.state = { body: [], nextId: 0, vars: [] };
     this.recurse(ast);
-    return new Function('s',this.state.body.join(''));
+    return new Function('s',
+(this.state.vars.length ?'var ' + this.state.vars.join(',') + ';' :'') + this.state.body.join(''));
 };
 
 ASTCompiler.prototype.recurse = function (ast) {
@@ -298,11 +299,26 @@ ASTCompiler.prototype.recurse = function (ast) {
             }, this);
             return '{' + properties.join(',') + '}';
         case AST.Identifier:
-            return this.nonComputedMember('s', ast.name);
+            var intoId = this.nextId();
+            this.if_('s', this.assign(intoId, this.nonComputedMember('s', ast.name)));
+            return intoId;
     }
 };
+ASTCompiler.prototype.nextId = function () {
+    var id = 'v' + (this.state.nextId++);
+    this.state.vars.push(id);
+    return id;
+};
 
-ASTCompiler.prototype.nonComputedMember = function(left,right) {
+ASTCompiler.prototype.assign = function (id, value) {
+    return id + '=' + value + ';';
+};
+
+ASTCompiler.prototype.if_ = function (test, consequent) {
+    this.state.body.push('if(', test, '){', consequent, '}');
+};
+
+ASTCompiler.prototype.nonComputedMember = function (left, right) {
     return '(' + left + ').' + right;
 }
 
@@ -323,6 +339,7 @@ ASTCompiler.prototype.escape = function (value) {
         return value;
     }
 };
+
 
 function Parser(lexer) {
     this.lexer = lexer;
