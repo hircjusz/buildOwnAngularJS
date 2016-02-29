3c4a6114440039ce5f2788e747aea97d98228f51
hircjusz <darek.uchman@gmail.com>
2016-02-29 17:51:13 +0100

    filter registration

---
+++ BuildOwnAngularJs/test/filter_spec.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="../Scripts/jasmine.js" />
+/// <reference path="../src/filter.js" />
+/// <reference path="../lib/loodash.js" />
+
+
+describe("filters", function() {
+    it('can be registered and obtained', function () {
+        var myFilter = function () { };
+        var myFilterFactory = function () {
+            return myFilter;
+        };
+        register('my', myFilterFactory);
+        expect(filter('my')).toBe(myFilter);
+    });
+
+    it('allows registering multiple filters with an object', function () {
+        var myFilter = function () { };
+        var myOtherFilter = function () { };
+        register({
+            my: function () {
+                return myFilter;
+            },
+            myOther: function () {
+                return myOtherFilter;
+            }
+        });
+        expect(filter('my')).toBe(myFilter);
+        expect(filter('myOther')).toBe(myOtherFilter);
+    });
+});
