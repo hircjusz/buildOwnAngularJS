2b34ff3601b61c24735225bd5ce099cb60bbdc16
hircjusz <darek.uchman@gmail.com>
2016-03-05 16:00:08 +0100

    negated filtering with string

--- BuildOwnAngularJs/src/filter.js
+++ BuildOwnAngularJs/src/filter.js
@@ -20,7 +20,10 @@ function filter(name) {
     return filters[name];
 }
 
- function deepCompare(actual, expected, comparator) {
+function deepCompare(actual, expected, comparator) {
+    if (_.isUndefined(actual)) {
+        return false;
+    }
         if (_.isObject(actual)) {
             return _.some(actual, function (value) {
                 return deepCompare(value, expected, comparator);
@@ -36,8 +39,11 @@ function createPredicateFn(expression) {
    
 
     function comparator(actual, expected) {
-        actual = actual.toLowerCase();
-        expected = expected.toLowerCase();
+        if (_.isNull(actual) || _.isNull(expected)) {
+            return actual === expected;
+        }
+        actual = ('' + actual).toLowerCase();
+        expected = ('' + expected).toLowerCase();
         return actual.indexOf(expected) !== -1;
     }
 
@@ -52,12 +58,14 @@ function filterFilter() {
         var predicateFn;
         if (_.isFunction(filterExpr)) {
             predicateFn = filterExpr;
-        } else if (_.isString(filterExpr)) {
+        } else if (_.isString(filterExpr) ||
+        _.isNumber(filterExpr) ||
+        _.isBoolean(filterExpr)||
+           _.isNull(filterExpr)) {
             predicateFn = createPredicateFn(filterExpr);
         } else {
             return array;
         }
-
         return _.filter(array, predicateFn);
     };
 }
