fe865c792e91e1c22e22563b1ff200c3f98efa94
hircjusz <darek.uchman@gmail.com>
2016-05-29 19:06:29 +0200

    injector provider

--- BuildOwnAngularJs/src/injector.js
+++ BuildOwnAngularJs/src/injector.js
@@ -1,7 +1,7 @@
 ﻿/// <reference path="../lib/loodash.js" />
 'use strict';
 
-function createInjector(modulesToLoad,strictDI) {
+function createInjector(modulesToLoad, strictDI) {
     var cache = {};
     var loadedModules = {};
     var providerCache = {};
@@ -10,11 +10,22 @@ function createInjector(modulesToLoad,strictDI) {
 
     function getService(name) {
         if (instanceCache.hasOwnProperty(name)) {
+            if (instanceCache[name] === INSTANTIATING) {
+                throw new Error('Circular dependency found');
+            }
             return instanceCache[name];
         }
         else if (providerCache.hasOwnProperty(name + 'Provider')) {
-            var provider = providerCache[name + 'Provider'];
-            return invoke(provider.$get,provider);
+            instanceCache[name] = INSTANTIATING;
+            try {
+                var provider = providerCache[name + 'Provider'];
+                var instance = instanceCache[name] = invoke(provider.$get);
+                return instance;
+            } finally {
+                if (instanceCache[name] === INSTANTIATING) {
+                    delete instanceCache[name];
+                }
+            }
         }
     };
 
@@ -27,29 +38,29 @@ function createInjector(modulesToLoad,strictDI) {
 
             instanceCache[key] = value;
         },
-        provider: function(key, provider) {
+        provider: function (key, provider) {
             providerCache[key + 'Provider'] = provider;
         }
     };
 
-    function instantiate(Type,locals)
-    {
+    function instantiate(Type, locals) {
         var UnwrappedType = _.isArray(Type) ? _.last(Type) : Type;
         var instance = Object.create(UnwrappedType.prototype);
-        invoke(Type, instance,locals);
+        invoke(Type, instance, locals);
         return instance;
     }
 
     var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
     var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
     var STRIP_COMMENTS = /(\/\/.*$)|(\/\*.*?\*\/)/mg;
+    var INSTANTIATING = {};
 
     function annotate(fn) {
         if (_.isArray(fn)) {
-            return fn.slice(0,fn.length-1);
-        } else if(fn.$inject){
+            return fn.slice(0, fn.length - 1);
+        } else if (fn.$inject) {
             return fn.$inject;
-        }else if (!fn.length) {
+        } else if (!fn.length) {
             return [];
         } else {
             if (strictDi) {
@@ -64,21 +75,21 @@ function createInjector(modulesToLoad,strictDI) {
         }
     }
 
-    function invoke(fn,self,locals) {
+    function invoke(fn, self, locals) {
 
         var args = _.map(annotate(fn), function (token) {
             if (_.isString(token)) {
                 return locals && locals.hasOwnProperty(token) ?
                     locals[token] : getService(token);
             } else {
-                throw 'Incorrect injected token. Expected string but got '+token;
+                throw 'Incorrect injected token. Expected string but got ' + token;
             }
         });
 
         if (_.isArray(fn)) {
             fn = _.last(fn);
         }
-        return  fn.apply(self, args);
+        return fn.apply(self, args);
     };
 
     _.forEach(modulesToLoad, function loadModule(moduleName) {
@@ -86,7 +97,7 @@ function createInjector(modulesToLoad,strictDI) {
             loadedModules[moduleName] = true;
             var module = window.angular.module(moduleName);
             _.forEach(module.requires, loadModule);
-            _.forEach(module._invokeQueue, function(invokeArgs) {
+            _.forEach(module._invokeQueue, function (invokeArgs) {
 
                 var method = invokeArgs[0];
                 var args = invokeArgs[1];
@@ -98,14 +109,14 @@ function createInjector(modulesToLoad,strictDI) {
 
 
     return {
-        has: function(key) {
+        has: function (key) {
             return instanceCache.hasOwnProperty(key) ||
-                providerCache.hasOwnProperty(Key + 'Provider');
+                providerCache.hasOwnProperty(key + 'Provider');
         },
         get: getService,
         invoke: invoke,
         annotate: annotate,
         instantiate: instantiate
-        
+
     };
 }
