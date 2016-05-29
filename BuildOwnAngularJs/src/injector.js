ae2079e1460e8d55c9ed3a34b61d8b0c483a2fc8
hircjusz <darek.uchman@gmail.com>
2016-05-29 16:10:44 +0200

    injector: constant

--- BuildOwnAngularJs/src/injector.js
+++ BuildOwnAngularJs/src/injector.js
@@ -2,5 +2,30 @@
 'use strict';
 
 function createInjector(modulesToLoad) {
-    return {};
+    var cache = {};
+
+    var $provide = {
+        constant: function(key, value) {
+            cache[key] = value;
+        }
+    };
+
+    _.forEach(modulesToLoad, function(moduleName) {
+
+        var module = window.angular.module(moduleName);
+        _.forEach(module._invokeQueue, function(invokeArgs) {
+            
+            var method = invokeArgs[0];
+            var args = invokeArgs[1];
+            $provide[method].apply($provide, args);
+
+        });
+    });
+
+
+    return {
+        has: function(key) {
+            return cache.hasOwnProperty(key);
+        }
+    };
 }
--- BuildOwnAngularJs/src/loader.js
+++ BuildOwnAngularJs/src/loader.js
@@ -8,10 +8,15 @@
         if (name === 'hasOwnProperty') {
             throw 'hasOwnProperty is not a valid module name';
         }
+        var invokeQueue = [];
 
         var moduleInstance = {
             name: name,
-            requires: requires
+            requires: requires,
+            constant: function(key, value) {
+                invokeQueue.push(['constant',[key,value]]);
+            },
+            _invokeQueue:invokeQueue
         };
         modules[name] = moduleInstance;
         return moduleInstance;
