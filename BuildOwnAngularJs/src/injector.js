2a303ea26af0f2f2eada8f4391a829db5380b358
hircjusz <darek.uchman@gmail.com>
2016-05-29 16:26:01 +0200

    modules dependent on other modules

--- BuildOwnAngularJs/src/injector.js
+++ BuildOwnAngularJs/src/injector.js
@@ -3,29 +3,40 @@
 
 function createInjector(modulesToLoad) {
     var cache = {};
+    var loadedModules = {};
 
     var $provide = {
-        constant: function(key, value) {
+        constant: function (key, value) {
+            if (key === 'hasOwnProperty') {
+                throw 'hasOwnProperty is not a valid constant name';
+            }
+
             cache[key] = value;
         }
     };
 
-    _.forEach(modulesToLoad, function(moduleName) {
+    _.forEach(modulesToLoad, function loadModule(moduleName) {
+        if (!loadedModules.hasOwnProperty(moduleName)) {
+            loadedModules[moduleName] = true;
+            var module = window.angular.module(moduleName);
+            _.forEach(module.requires, loadModule);
+            _.forEach(module._invokeQueue, function(invokeArgs) {
 
-        var module = window.angular.module(moduleName);
-        _.forEach(module._invokeQueue, function(invokeArgs) {
-            
-            var method = invokeArgs[0];
-            var args = invokeArgs[1];
-            $provide[method].apply($provide, args);
+                var method = invokeArgs[0];
+                var args = invokeArgs[1];
+                $provide[method].apply($provide, args);
 
-        });
+            });
+        }
     });
 
 
     return {
         has: function(key) {
             return cache.hasOwnProperty(key);
+        },
+        get: function(key) {
+            return cache[key];
         }
     };
 }
