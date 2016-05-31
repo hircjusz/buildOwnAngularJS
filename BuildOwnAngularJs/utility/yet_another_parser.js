76f5c9991077cf4e3c5f321b42e69ae3c706f4a9
hircjusz <darek.uchman@gmail.com>
2016-05-31 21:59:00 +0200

    yap tests

--- BuildOwnAngularJs/utility/yet_another_parser.js
+++ BuildOwnAngularJs/utility/yet_another_parser.js
@@ -1,15 +1,182 @@
 ﻿/// <reference path="../lib/loodash.js" />
 
-function isWhiteSpace(ch) {
+
+function Scanner(expression) {
+    this.expression = expression;
+    this.index = 0;
+    this.length = expression ? expression.length : 0;
+}
+
+Scanner.prototype.isWhiteSpace= function(ch) {
     return (ch === 'u0009')
         || (ch === ' ')
         || (ch === 'u00A0');
 }
 
-function isLetter(ch) {
+//function isWhiteSpace(ch) {
+//    return (ch === 'u0009')
+//        || (ch === ' ')
+//        || (ch === 'u00A0');
+//}
+
+Scanner.prototype.isLetter=function(ch) {
     return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
 }
 
-function isDecimalDigit(ch) 
+Scanner.prototype.isDecimalDigit=function(ch)
 { return (ch >= '0') && (ch <= '9'); }
+
+
+Scanner.prototype.createToken=function(type, value) {
+    return {
+        type: type,
+        value: value
+    }
+}
+
+Scanner.prototype.getNextChar= function() {
+    var ch = undefined,
+        idx = this.index;
+    if (idx < this.length) {
+        ch = this.expression.charAt(idx);
+        this.index += 1;
+    }
+    return ch;
+}
+
+Scanner.prototype.peekNextChar= function() {
+    var idx = this.index;
+    return ((idx < this.length) ? this.expression.charAt(idx) : undefined);
+
+}
+
+
+Scanner.prototype.skipSpaces= function() {
+    var ch;
+    while (this.index < this.length) {
+        ch = this.peekNextChar();
+        if (!this.isWhiteSpace(ch)) {
+            break;
+        }
+        this.getNextChar();
+    }
+}
+
+Scanner.prototype.scanOperator= function() {
+    var ch = this.peekNextChar();
+    if ('+-*/()'.indexOf(ch) >= 0) {
+        return this.createToken('Operator',this.getNextChar());
+    }
+    return undefined;
+}
+
+Scanner.prototype.isIdentifierStart=function (ch) {
+    return (ch === '_') || this.isLetter(ch);
+}
+
+Scanner.prototype.isIdentifierPart= function(ch) {
+    return this.isIdentifierStart(ch) || this.isDecimalDigit(ch);
+}
+
+Scanner.prototype.scanIdentifier= function() {
+    var ch, id;
+    ch = this.peekNextChar();
+
+    if (!this.isIdentifierStart(ch)) {
+        return undefined;
+    }
+    id = this.getNextChar();
+    while (true) {
+        ch = this.peekNextChar();
+        if (!this.isIdentifierPart(ch)) {
+            break;
+        }
+        id+= this.getNextChar();
+
+    }
+    return this.createToken('Identifier',id);
+}
+
+Scanner.prototype.scanNumber= function() {
+    var ch = this.peekNextChar();
+
+    if (!this.isDecimalDigit(ch) && (ch !== '.')) {
+        return undefined;
+    }
+    var isInt = false;
+
+    var number = '';
+    if (ch !== '.') {
+        isInt = true;
+        number = this.getNextChar();
+        while (true) {
+            ch = this.peekNextChar();
+            if (!this.isDecimalDigit(ch)) {
+                break;
+            }
+            number += this.getNextChar();
+        }
+    }
+
+    if (ch === '.') {
+        isInt = false;
+        number += ch;
+        ch = this.getNextChar();
+        while (true) {
+            ch = this.peekNextChar();
+            if (!this.isDecimalDigit(ch)) {
+                break;
+            }
+            number += this.getNextChar();
+        }
+    }
+
+    if (ch === 'e' || ch === 'E') {
+        isInt = false;
+        number += this.getNextChar();
+        ch = this.peekNextChar();
+        if (ch === '+' || ch === '-' || this.isDecimalDigit(ch)) {
+            number += this.getNextChar();
+            while (true) {
+                ch = this.peekNextChar();
+                if (!this.isDecimalDigit(ch)) {
+                    break;
+                }
+                number += this.getNextChar();
+            }
+        } else {
+            throw new SyntaxError('Unexpected character after exponent sign');
+        }
+
+    }
+      return this.createToken('Number',isInt?parseInt(number):parseFloat(number));
+
+}
+
+Scanner.prototype.next= function() {
+    var token;
+
+    this.skipSpaces();
+    if (this.index >= this.length) {
+        return undefined;
+    }
+
+    token = this.scanNumber();
+    if (typeof token !== 'undefined') {
+        return token;
+    }
+
+    token = this.scanOperator();
+    if (typeof token !== 'undefined') {
+        return token;
+    }
+
+    token = this.scanIdentifier();
+    if (typeof token !== 'undefined') {
+        return token;
+    }
+    throw new SyntaxError('Unknown token '+this.peekNextChar());
+
+}
+
 
--- BuildOwnAngularJs/utility/yet_another_parser_test.js
+++ BuildOwnAngularJs/utility/yet_another_parser_test.js
@@ -2,22 +2,185 @@
 /// <reference path="../Scripts/jasmine.js" />
 /// <reference path="../utility/yet_another_parser.js" />
 
-describe("Yet another parser", function() {
+describe("Yet another parser", function () {
 
-    describe("parserT", function() {
-        
+    var scanner;
+    beforeEach(function () {
+        scanner = new Scanner();
+    });
+    describe("parserT", function () {
+
         it("function isWhiteSpace", function () {
 
-            expect(isWhiteSpace(' ')).toBe(true);
+
+            expect(scanner.isWhiteSpace(' ')).toBe(true);
         });
 
         it("function isDecimal", function () {
 
-            expect(isDecimalDigit('9')).toBe(true);
+            expect(scanner.isDecimalDigit('9')).toBe(true);
+        });
+
+        it("function createToken", function () {
+
+            var token = scanner.createToken('int', 9);
+
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'int',
+                value: 9
+            }));
         });
 
 
+        it("function getNextChar 2-1", function () {
+            scanner = new Scanner('abcdef');
+            expect(scanner.index).toBe(0);
+            expect(scanner.length).toBe(6);
+            var ch=scanner.getNextChar();
+            expect(ch).toBe('a');
+            ch = scanner.getNextChar();
+            expect(ch).toBe('b');
+        });
 
+        it("function peekNextChar-2", function () {
+            scanner = new Scanner('abcdef');
+            expect(scanner.index).toBe(0);
+            expect(scanner.length).toBe(6);
+            var ch = scanner.peekNextChar();
+            expect(ch).toBe('a');
+            ch = scanner.peekNextChar();
+            expect(ch).toBe('a');
+        });
+
+        it("function skipSpaces", function () {
+            scanner = new Scanner('    abcdef');
+            expect(scanner.index).toBe(0);
+            expect(scanner.length).toBe(10);
+            var ch = scanner.skipSpaces();
+            expect(scanner.index).toBe(4);
+            
+        });
+
+        it("function scanOperator", function () {
+            scanner = new Scanner('+-*/()');
+            var token = scanner.scanOperator();
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Operator',
+                value: '+'
+            }));
+            token = scanner.scanOperator();
+
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Operator',
+                value: '-'
+            }));
+
+            token = scanner.scanOperator();
+
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Operator',
+                value: '*'
+            }));
+
+            token = scanner.scanOperator();
+
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Operator',
+                value: '/'
+            }));
+            token = scanner.scanOperator();
+
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Operator',
+                value: '('
+            }));
+            token = scanner.scanOperator();
+
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Operator',
+                value: ')'
+            }));
+        });
+
+        it("function scanIdentifier", function () {
+            scanner = new Scanner('alpha');
+            var token = scanner.scanIdentifier();
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Identifier',
+                value: 'alpha'
+            }));
+            scanner = new Scanner('a190');
+             token = scanner.scanIdentifier();
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Identifier',
+                value: 'a190'
+            }));
+
+
+        });
+
+        it("function scanNumber Integer", function () {
+            scanner = new Scanner('20000');
+            var token= scanner.scanNumber();
+
+        });
+
+        it("function scanNumber Float", function () {
+            scanner = new Scanner('213.23');
+            var token = scanner.scanNumber();
+        });
+        it("function scanNumber exponent", function () {
+            scanner = new Scanner('213e5');
+            var token = scanner.scanNumber();
+        });
+
+
+        it("function next exponent", function () {
+            scanner = new Scanner('213e5 aaa a190 213.23 / *  -');
+            var token;
+            token = scanner.next();
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Number',
+                value: 21300000
+            }));
+
+            token = scanner.next();
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Identifier',
+                value: 'aaa'
+            }));
+
+            token = scanner.next();
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Identifier',
+                value: 'a190'
+            }));
+
+            token = scanner.next();
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Number',
+                value: 213.23
+            }));
+
+           
+            token = scanner.next();
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Operator',
+                value: '/'
+            }));
+
+            token = scanner.next();
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Operator',
+                value: '*'
+            }));
+
+           token = scanner.next();
+            expect(token).toEqual(jasmine.objectContaining({
+                type: 'Operator',
+                value: '-'
+            }));
+        });
 
     });
 
