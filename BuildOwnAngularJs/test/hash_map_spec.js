7aba063cef96d243c89d380bc11a2bc3f9de865f
hircjusz <darek.uchman@gmail.com>
2016-05-31 19:41:28 +0200

    hash map infractructure

---
+++ BuildOwnAngularJs/test/hash_map_spec.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../Scripts/jasmine.js" />
+/// <reference path="../lib/loodash.js" />
+/// <reference path="../src/hash_map.js" />
+
+describe('hash', function () {
+    'use strict';
+    describe('hashKey', function () {
+        it('is undefined:undefined for undefined', function () {
+            expect(hashKey(undefined)).toEqual('undefined:undefined');
+        });
+    });
+});
