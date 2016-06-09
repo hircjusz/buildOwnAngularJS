75488e7af29845d9668ae4346d08851a299b3cdd
hircjusz <darek.uchman@gmail.com>
2016-06-09 22:41:32 +0200

    angular public API

---
+++ BuildOwnAngularJs/src/anguar_public.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="../Scripts/jasmine.js" />
+/// <reference path="../lib/loodash.js" />
+/// <reference path="../src/loader.js" />
+
+
+function publishExternalAPI() {
+    setupModuleLoader(window);
+}
+
---
+++ BuildOwnAngularJs/test/angular_public_spec.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="../Scripts/jasmine.js" />
+/// <reference path="../lib/loodash.js" />
+/// <reference path="../src/loader.js" />
+/// <reference path="../src/anguar_public.js" />
+
+
+'use strict';
+
+describe('angularPublic', function () {
+    it('sets up the angular object and the module loader', function () {
+        publishExternalAPI();
+        expect(window.angular).toBeDefined();
+        expect(window.angular.module).toBeDefined();
+    });
+});
